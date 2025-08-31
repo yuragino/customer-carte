50e63a0024b4e5c20bbf3a4ef738e9909b369619
yuragino <ayapeko2525@gmail.com>
2025-08-31 17:11:50 +0900

    修正

--- js/form.js
+++ js/form.js
@@ -236,10 +236,6 @@ document.addEventListener('alpine:init', () => {
         additionalRentals: [],
         imagePreviews: [], // 画面表示用のプレビューURL
         imageFiles: [],    // アップロード用のFileオブジェクト
-        // // schedule.jsで使用するプロパティ
-        // status: '受付完了',
-        // booth: '',
-        // staff: '',
         statusTimestamps: {
           receptionCompletedAt: null,
           guidanceCompletedAt: null,
