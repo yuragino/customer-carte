3ad0fb7b2bdb99578c1c059a7ae993133fe3356d
yuragino <ayapeko2525@gmail.com>
2026-01-10 18:04:33 +0900

    ログイン機能実装

--- assets/js/top-page.js
+++ assets/js/top-page.js
@@ -1,14 +1,18 @@
 import { getYearSettings } from "./common/year-selector.js";
 import { signInWithGoogle, initAuthState, signOutGoogle } from "./common/firebase-auth.js";
+
 document.addEventListener('alpine:init', () => {
   Alpine.data('app', () => ({
     ...getYearSettings(),
     user: null,
+    isLoading: true,
 
     init() {
       this.initYearSelector();
+      // Firebase Auth 状態監視
       initAuthState((firebaseUser) => {
         this.user = firebaseUser;
+        this.isLoading = false;
       });
     },
 
@@ -17,8 +21,8 @@ document.addEventListener('alpine:init', () => {
     },
 
     logout() {
+      confirm("ログアウトしますか?");
       signOutGoogle();
     }
-
   }));
 });
